8d56d992f37dfb1247e5975548a301f948ab5cc3
phiphu2002 <phiphu@gmail.com>
2017-09-13 19:47:53 +0700

    Delete SocketIoClient_2000.js

--- SocketIoAri/SocketIoClient_2000.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env nodejs
-
-var io = require('socket.io-client');
-var client = io.connect('http://192.168.1.9:8089');
-var msg = '2000';
-client.emit('extension', msg);
-client.on('callerid', function(data){
-    console.log(data);
-});
